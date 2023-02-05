7222d8a7362a66f1f776711a99223572e2f74397
sowmik-sec <sowmik.sec@gmail.com>
2023-02-05 21:52:14 +0600

    methods explored

---
+++ methods.js
@@ -0,0 +1,24 @@
+const student = {
+  name: "Ahsan Habib",
+  money: 0,
+  study: "CSE",
+  subjects: ["Data Structure", "Algorithm", "Discrete Math"],
+  exam: function () {
+    return this.name + ` is not participating in an exam`;
+  },
+  improveExam: function (subject) {
+    this.exam();
+    return `${this.name} is taking improvement exam on ${subject}`;
+  },
+  treatDey: function (amount) {
+    this.money = this.money - amount;
+    return this.money;
+  },
+};
+
+// console.log(student.exam());
+const reExam = student.improveExam("Bangla");
+//console.log(reExam);
+
+const remaining = student.treatDey(200);
+console.log(remaining);
